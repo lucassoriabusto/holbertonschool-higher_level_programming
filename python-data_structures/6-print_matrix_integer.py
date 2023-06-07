ef7b90cf426cd54ed90252342753e9483edb5288
lucassoriabusto <lucassoria@hotmail.com>
2023-06-07 10:10:58 -0700

    Task 06

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         return
     for row in matrix:
         for element in row:
-            print(element, end=" ")
+            print("{:d}".format(element), end=" ")
         print()
