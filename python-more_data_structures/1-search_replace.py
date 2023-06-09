c1fa94fe043443c1b1dfcc54748db4b9a90fc5df
lucassoriabusto <lucassoria@hotmail.com>
2023-06-09 09:10:08 -0700

    Task 01

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list == None:
-        return my_list
-    else:
-        new_list = []
-        for i in my_list:
-            if i == search:
-                new_list.append(replace)
-            else:
-                new_list.append(i)
-        return new_list
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
