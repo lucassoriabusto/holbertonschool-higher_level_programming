1542d1b86b62d6ec16314568d5ed335502ad4893
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-06-17 14:33:10 -0700

    Task 05

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function that prints a text with 2 new lines
+after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """Checks if text is a string and prints its content
+    skipping the spaces after ".", "?", ":"."""
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    else:
+        result = ""
+        i = 0
+        while i < len(text):
+            if text[i] in (".", "?", ":"):
+                result += text[i]
+                if i < len(text) - 1 and text[i+1] == " ":
+                    i += 1
+                result += "\n\n"
+            else:
+                result += text[i]
+            i += 1
+
+        print(result)
