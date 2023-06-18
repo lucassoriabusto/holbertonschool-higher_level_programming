9ee0636a4962ccf601ee030589081959e3643f02
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-06-18 13:42:28 -0700

    Task 01

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """The case is defined as rectangle"""
     def __init__(self, width=0, height=0):
         """Defines a private instance attribute."""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
