426b14307b388a0cc2982b9336ed06749d43aef0
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-06-18 19:11:45 -0700

    Task 09

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -91,5 +91,6 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """Class method that returns a new Rectangle instance with width == height == size."""
+        """Class method that returns a new Rectangle
+        instance with width == height == size."""
         return cls(size, size)
