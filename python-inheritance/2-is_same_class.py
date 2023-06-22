bdf4fe971c33e0b6329d2c67a703b171f6d675f4
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-06-22 13:08:09 -0700

    Task 02

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Check if the object is exactly an instance of the specified clas."""
 
+
 def is_same_class(obj, a_class):
     """If it is an instance return True, otherwise False."""
     if type(obj) == a_class:
