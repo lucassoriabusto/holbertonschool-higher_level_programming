05f79e880d1e74d0e3c7c2a03746dae13a8edebb
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-06-22 14:15:40 -0700

    Task 05-06

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """The case is defined as BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
