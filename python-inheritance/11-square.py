a228ae9120301a49831c35085dc1da1767fe4413
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-06-22 18:55:24 -0700

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write an empty class BaseGeometry."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class Squear that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialization the size."""
+        self.__size = size
+
+        self.integer_validator("size", size)
+
+    def area(self):
+        """Returns the area."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Prints the dimensions of the square."""
+        return(f"[Rectangle] {self.__size}/{self.__size}")
