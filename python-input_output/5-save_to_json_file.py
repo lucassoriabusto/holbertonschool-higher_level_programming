c3a209ec154804be488b8b6ff762c9b785893a04
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-06-23 20:48:49 -0700

    Task 05

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,4 +11,4 @@ def save_to_json_file(my_obj, filename):
     json_obj = json.dumps(my_obj)
     with open(filename, "w", encoding="utf-8") as f:
         """Escribir la cadena JSON en el archivo"""
-        f.write(json_obj)    
+        f.write(json_obj)
