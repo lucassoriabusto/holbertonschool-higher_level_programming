77375791388b34c8175cce62f0b55db38902affc
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-06-30 10:50:38 -0700

    Task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,3 +17,17 @@ class Square(Rectangle):
     def __str__(self):
         """String representation of Square."""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
+
+    @property
+    def size(self):
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+            self.__height = value
