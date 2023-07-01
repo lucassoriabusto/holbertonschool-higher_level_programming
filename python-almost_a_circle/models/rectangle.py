8c61894206a3bbfc66712fb85c769b83b8918be2
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-07-01 11:47:00 -0700

    Task 12

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,8 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square."""
+        return {"x": self.x, "y": self.y, "id": self.id,
+                "height": self.height, "width": self.width}
