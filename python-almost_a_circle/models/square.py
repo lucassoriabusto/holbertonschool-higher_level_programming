4ab57ce1a0024c1bc0c087e0388107c8179cf31a
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-07-01 13:32:08 -0700

    Task 14

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -46,4 +46,4 @@ class Square(Rectangle):
     def to_dictionary(self):
         """Returns the dictionary representation of a Square."""
         return {"id": self.id, "size": self.size,
-                "x": self.x, "y": self.y,}
+                "x": self.x, "y": self.y}
