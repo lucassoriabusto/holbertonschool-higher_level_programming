cee78926f4028c1bfeca9f6fc7004d1a2865c295
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-07-01 18:15:58 -0700

    Task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,27 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Saves the JSON representation of a list of objects in a f
+
+        cls (class): La clase misma (Base) como parámetro implícito.
+        list_objs (list): Lista de objetos heredados de la clase Base."""
+        if list_objs is None:
+            list_objs = []
+
+        """Obtiene el nombre de la clase y le agrega la extensión .json."""
+        class_name = cls.__name__
+        filename = f"{class_name}.json"
+
+        """Itera sobre la lista de objetos y
+        obtiene el diccionario de cada objeto."""
+        obj_dicts = []
+        for obj in list_objs:
+            obj_dicts.append(obj.to_dictionary())
+
+        """Convierte la lista de diccionarios en una cadena JSON."""
+        json_string = cls.to_json_string(obj_dicts)
+        with open(filename, "w") as file:
+            file.write(json_string)
