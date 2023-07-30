65e69760a0165200c013c6b8e7c7a801a8140739
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-07-30 11:53:01 -0700

    Task 08

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists all the cities of California.
-SELECT cities.state_id
+SELECT cities.state_id, cities.name
 FROM cities, states
-WHERE cities.state_id = states.id AND states.name = 'California'
+WHERE cities.state_id = states.id AND states.name = "California"
 ORDER BY cities.id ASC;
